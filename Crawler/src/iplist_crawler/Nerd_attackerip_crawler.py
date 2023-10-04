c5cddd2cc543e9af14e2d36a2a4c4ea701d3614a
ssoulistic@pangyo <ssoulistic@gmail.com>
2023-10-04 14:41:31 +0000

    hint

--- Crawler/src/iplist_crawler/Nerd_attackerip_crawler.py
+++ Crawler/src/iplist_crawler/Nerd_attackerip_crawler.py
@@ -1,16 +1,13 @@
 import requests
 from datetime import datetime
 from tqdm import tqdm
-
 import sys, os
 sys.path.append('/home/teamlab/ThanosIP/Module/')
 import IPmaster
 import dbModule
-# from IPmaster import ip_shredder
-# from dbModule import Database
-
 
 shred_inst=IPmaster.ip_shredder()
+
 url = 'https://nerd.cesnet.cz/nerd/data/bad_ips.txt'
 response = requests.get(url)
 
@@ -20,10 +17,23 @@ if response.status_code == 200:  # '200'은 HTTP 상태 코드 중 하나로, 'O
 	table_name="bad_ip_list" # 사용할 Table 이름
 	DB_name="laBelup" # 사용할 Database 이름
 	db_class = dbModule.Database(DB_name) # db 인스턴스
+	crawled_ip_list = tqdm(ip_list)
+	
+	db_class.execute(f"SELECT ip FROM {table_name}") 
+	
+	print(ipInDB,"ipInDB")
+	print(crawled_ip_list,"ip_list")
 	for i in tqdm(ip_list):
 		insert_data=f'"{i}","{url}","{current_datetime}"'
-		sql = f"INSERT INTO {table_name} VALUES({insert_data})" 
+		print(i,"first i")
+		if i in ipInDB:
+			sql = f"INSERT INTO {table_name} VALUES({insert_data})" 
+			print(i,"if_i")
+		else:
+			sql = f"UPDATE {DB_name} SET from = {url}, update_time = {current_datetime}"
+			print(i,"else_if")
 		db_class.execute(sql) # db에 sql문 작성
 		db_class.commit() #sql문 실행 
+
 else:
     print(f"파일을 다운로드하는 중에 오류가 발생했습니다. 응답 코드: {response.status_code}")
